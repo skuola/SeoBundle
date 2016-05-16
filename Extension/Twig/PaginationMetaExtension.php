e619fe2235d422f4c3a79ad691fcbf99ca7d813c
odimodugno <oreste.dimodugno@gmail.com>
2016-05-16 17:23:56 +0200

    Simplified funcionality

--- Extension/Twig/PaginationMetaExtension.php
+++ Extension/Twig/PaginationMetaExtension.php
@@ -3,136 +3,84 @@
 namespace Skuola\SeoBundle\Extension\Twig;
 
 use Knp\Bundle\PaginatorBundle\Pagination\SlidingPagination;
-use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\Generator\UrlGenerator;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\PropertyAccess\PropertyAccess;
-use Symfony\Component\HttpFoundation\Request;
 
 class PaginationMetaExtension extends \Twig_Extension
 {
+    const NEXT = 'next';
+    const PREV = 'prev';
+
     /**
      * @var RouterInterface
      */
     protected $router;
 
     /**
-     * @var null|Request
+     * @var PropertyAccess
      */
-    protected $request;
+    protected $accessor;
 
-    /**
-     * @var string
-     */
-    protected $domain;
-
-    public function __construct(RequestStack $requestStack, RouterInterface $router, $domain)
+    public function __construct(RouterInterface $router)
     {
         $this->router = $router;
-        $this->domain = $domain;
-        $this->request = $requestStack->getMasterRequest();
+        $this->accessor = PropertyAccess::createPropertyAccessor();
     }
 
     public function getFunctions()
     {
         return [
             new \Twig_SimpleFunction('render_pagination_meta', [$this, 'renderPaginationMeta'], ['is_safe' => ['html']]),
-            new \Twig_SimpleFunction('render_pagination_canonical', [$this, 'renderPaginationCanonical'], ['is_safe' => ['html']]),
-            new \Twig_SimpleFunction('generate_pagination_robots_meta_tag', [$this, 'renderPaginationRobots'], ['is_safe' => ['html']]),
+            new \Twig_SimpleFunction('render_pagination_robots', [$this, 'renderPaginationRobots'], ['is_safe' => ['html']]),
         ];
     }
 
-    public function renderPaginationRobots($current_page_value)
+    public function renderPaginationRobots(SlidingPagination $pagination)
     {
-        $index_value = 'index'; $follow_value = 'follow';
+        $index_value = 'index';
+        $follow_value = 'follow';
 
-        if($current_page_value > 1){
+        if($pagination->getCurrentPageNumber() > 1){
             $index_value = 'noindex';
         }
 
         return sprintf('<meta name="robots" content="%s,%s" />', $index_value, $follow_value);
     }
 
-    public function renderPaginationCanonical($request, $query_string = true)
+    public function renderPaginationMeta(SlidingPagination $pagination)
     {
-        $canonical_url = $query_string ? $this->generateCanonicalUrl($request) : $request->getPathInfo();
-
-        return sprintf('<link href="%s" rel="canonical">', $canonical_url);
-    }
-
-    public function renderPaginationMeta($items)
-    {
-        if (!$items instanceof SlidingPagination) {
-            return;
-        }
-
-        $paginationData = $items->getPaginationData();
-
+        $paginationData = $pagination->getPaginationData();
         $paginationMetas = '';
-        $accessor = PropertyAccess::createPropertyAccessor();
 
-        $prev = (int) $accessor->getValue($paginationData, '[previous]');
-        $next = (int) $accessor->getValue($paginationData, '[next]');
+        $prev = (int) $this->accessor->getValue($paginationData, '[previous]');
+        $next = (int) $this->accessor->getValue($paginationData, '[next]');
 
         if ($prev !== 0) {
-            $paginationMetas = $this->generateMeta('prev', $items, $prev);
+            $paginationMetas = $this->generateMeta($pagination, self::PREV, $prev);
         }
 
         if ($next !== 0) {
-            $paginationMetas .= $this->generateMeta('next', $items, $next);
+            $paginationMetas .= $this->generateMeta($pagination, self::NEXT, $next);
         }
 
         return $paginationMetas;
     }
 
-    protected function generateMeta($direction, $items, $pageNumber)
+    protected function generateMeta(SlidingPagination $pagination, $direction, $page)
     {
-        $pageUrl = sprintf('http://%s%s', $this->domain, $this->request->getPathInfo());
+        $routeInfo['name'] = $pagination->getRoute();
 
-        $queryString = [];
-        parse_str($this->request->getQueryString(), $queryString);
-
-        if ($pageNumber <= 1) {
-            if (array_key_exists('page', $queryString)) {
-                unset($queryString['page']);
-            }
-
-            return sprintf('<link rel="%s" href="%s">',
-                $direction,
-                $this->buildUrl($queryString, $pageUrl, $items->getPaginatorOptions())
-            );
-        }
-
-        $queryString['page'] = $pageNumber;
+        $routeInfo['params'] = $pagination->getQuery(compact('page'));
 
         return sprintf('<link rel="%s" href="%s">',
             $direction,
-            $this->buildUrl($queryString, $pageUrl, $items->getPaginatorOptions())
+            $this->router->generate(
+                $routeInfo['name'],
+                $routeInfo['params'],
+                UrlGenerator::ABSOLUTE_URL
+            )
         );
-    }
-
-    protected function generateCanonicalUrl(Request $request)
-    {
-        $route = $request->get('_route');
-        $parameters = $request->get('_route_params');
-
-        $current_page = $request->get('page');
-
-        if ($current_page != 1) {
-            $parameters['page'] = $current_page;
-        }
-
-        return $this->router->generate($route, $parameters, true);
-    }
-
-    private function buildUrl(array $queryString, $pageUrl, $paginatorOptions)
-    {
-        foreach ($queryString as $parameter => $value) {
-            if ( ! array_search($parameter, $paginatorOptions)) {
-                unset($queryString[$parameter]);
-            }
-        }
-
-        return (count($queryString) >= 1) ? sprintf('%s?%s', $pageUrl, http_build_query($queryString)) : $pageUrl;
     }
 
     public function getName()
--- Tests/Unit/Extension/Twig/PaginationMetaExtensionTest.php
+++ Tests/Unit/Extension/Twig/PaginationMetaExtensionTest.php
@@ -4,27 +4,25 @@ namespace Skuola\SeoBundle\Tests\Extension;
 
 use Skuola\SeoBundle\Extension\Twig\PaginationMetaExtension;
 use Symfony\Component\Routing\Router;
-use Knp\Component\Pager\Pagination\SlidingPagination;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpFoundation\Request;
+use Knp\Bundle\PaginatorBundle\Pagination\SlidingPagination;
 use Mockery as m;
 
 class PaginationMetaExtensionTest extends \PHPUnit_Framework_TestCase
 {
-    protected $routerMock;
-    protected $requestStackMock;
-    protected $requestMock;
+    /**
+     * @var Router
+     */
+    protected $router;
+
+    /**
+     * @var PaginationMetaExtension
+     */
+    protected $extension;
 
     public function setUp()
     {
-        $this->routerMock = $this->getMock(Router::class, ['generate'], [], '', false);
-
-        $this->routerMock->expects($this->any())
-            ->method('generate')
-            ->with($this->equalTo('dummy_route'))
-            ->will($this->returnValue('/test/path-info'));
-
-        $this->requestStackMock = $this->getMock(RequestStack::class, ['getMasterRequest'], [], '', false);
+        $this->router = m::mock(Router::class);
+        $this->extension = new PaginationMetaExtension($this->router);
     }
 
     public function tearDown()
@@ -34,214 +32,124 @@ class PaginationMetaExtensionTest extends \PHPUnit_Framework_TestCase
 
     public function testExtensionGiveNullIfNoPages()
     {
-        $slidingPaginationMock = $this->getMock(SlidingPagination::class, array('getPaginationData'), [], '', false);
+        $pagination = m::mock(SlidingPagination::class);
 
-        $slidingPaginationMock->expects($this->any())
-            ->method('getPaginationData')
-            ->will($this->returnValue([]));
+        $pagination->shouldReceive('getPaginationData')
+                   ->once()
+                   ->andReturn([
+                       'previous' => '0',
+                       'next'     => '0'
+                   ]);
 
-        $requestMock = $this->getMock(Request::class, ['getPathInfo'], [], '', true);
-
-        $requestMock->expects($this->any())
-            ->method('getPathInfo')
-            ->will($this->returnValue('/test/path-info'));
-
-        $this->requestStackMock->expects($this->any())
-            ->method('getMasterRequest')
-            ->will($this->returnValue($requestMock));
-
-        $paginationMeta = new PaginationMetaExtension($this->requestStackMock, $this->routerMock, 'local.domain.it');
-
-        $this->assertEmpty($paginationMeta->renderPaginationMeta($slidingPaginationMock));
+        $this->assertEmpty($this->extension->renderPaginationMeta($pagination));
     }
 
     public function testExtensionGiveNextIfFirstPage()
     {
-        $slidingPaginationMock = $this->getMock('Knp\Bundle\PaginatorBundle\Pagination\SlidingPagination', array('getPaginationData', 'getParams', 'getRoute', 'getPaginatorOptions'), [], '', false);
+        $pagination = m::mock(SlidingPagination::class);
 
-        $slidingPaginationMock->expects($this->at(0))
-            ->method('getPaginationData')
-            ->will($this->returnValue(array('next' => 2)));
+        $pagination->shouldReceive('getPaginationData')
+            ->once()
+            ->andReturn([
+                'next'     => '2'
+            ]);
 
-        $slidingPaginationMock->expects($this->any())
-            ->method('getRoute')
-            ->will($this->returnValue('dummy_route'));
+        $pagination->shouldReceive('getRoute')
+            ->once()
+            ->andReturn('dummy_route');
 
-        $slidingPaginationMock->expects($this->any())
-            ->method('getPaginatorOptions')
-            ->will($this->returnValue([
-                'pageParameterName' => 'page',
-                'sortDirectionParameterName' => 'direction',
-            ]));
+        $pagination->shouldReceive('getQuery')
+            ->once()
+            ->andReturn([
+                'direction' => 'asc',
+                'sort'      => 'sorting'
+            ]);
 
-        $requestMock = $this->getMock(
-            'Symfony\Component\HttpFoundation\Request',
-            ['getPathInfo'],
-            [
-                [
-                    'testParameter' => 'test',
-                    'page'          => 1,
-                    'direction'     => 'desc'
-                ]
-            ],
-            '',
-            true
+        $this->router->shouldReceive('generate')
+            ->once()
+            ->andReturn('http://local.domain.it/test/path-info?page=2');
+
+        $this->assertEquals(
+            '<link rel="next" href="http://local.domain.it/test/path-info?page=2">',
+            $this->extension->renderPaginationMeta($pagination)
         );
-
-        $requestMock->expects($this->any())
-            ->method('getPathInfo')
-            ->will($this->returnValue('/test/path-info'));
-
-        $this->requestStackMock->expects($this->any())
-            ->method('getMasterRequest')
-            ->will($this->returnValue($requestMock));
-
-        $paginationMeta = new PaginationMetaExtension($this->requestStackMock, $this->routerMock, 'local.domain.it');
-
-        $this->assertEquals('<link rel="next" href="http://local.domain.it/test/path-info?page=2">', $paginationMeta->renderPaginationMeta($slidingPaginationMock));
     }
 
     public function testExtensionGivePrevIfLastPage()
     {
-        $slidingPaginationMock = $this->getMock('Knp\Bundle\PaginatorBundle\Pagination\SlidingPagination', array('getPaginationData', 'getParams', 'getRoute', 'getPaginatorOptions'), [], '', false);
+        $pagination = m::mock(SlidingPagination::class);
 
-        $slidingPaginationMock->expects($this->at(0))
-            ->method('getPaginationData')
-            ->will($this->returnValue(array('previous' => 1)));
+        $pagination->shouldReceive('getPaginationData')
+            ->once()
+            ->andReturn([
+                'previous' => '9',
+                'next'     => '0'
+            ]);
 
-        $slidingPaginationMock->expects($this->any())
-            ->method('getRoute')
-            ->will($this->returnValue('dummy_route'));
+        $pagination->shouldReceive('getRoute')
+            ->once()
+            ->andReturn('dummy_route');
 
-        $slidingPaginationMock->expects($this->any())
-            ->method('getPaginatorOptions')
-            ->will($this->returnValue([
-                'pageParameterName' => 'page'
-            ]));
+        $pagination->shouldReceive('getQuery')
+            ->once()
+            ->andReturn([
+                'direction' => 'asc',
+                'sort'      => 'sorting'
+            ]);
 
-        $requestMock = $this->getMock(
-            'Symfony\Component\HttpFoundation\Request',
-            ['getPathInfo'],
-            [
-                [
-                    'testParameter' => 'test',
-                    'page'          => 2
-                ]
-            ],
-            '',
-            true
+        $this->router->shouldReceive('generate')
+            ->once()
+            ->andReturn('http://local.domain.it/test/path-info?page=9');
+
+        $this->assertEquals(
+            '<link rel="prev" href="http://local.domain.it/test/path-info?page=9">',
+            $this->extension->renderPaginationMeta($pagination)
         );
-
-        $requestMock->expects($this->any())
-            ->method('getPathInfo')
-            ->will($this->returnValue('/test/path-info'));
-
-        $this->requestStackMock->expects($this->any())
-            ->method('getMasterRequest')
-            ->will($this->returnValue($requestMock));
-
-        $paginationMeta = new PaginationMetaExtension($this->requestStackMock, $this->routerMock, 'local.domain.it');
-
-        $this->assertEquals('<link rel="prev" href="http://local.domain.it/test/path-info">', $paginationMeta->renderPaginationMeta($slidingPaginationMock));
     }
 
     public function testExtensionGivePrevAndNextInBetweenPages()
     {
-        $slidingPaginationMock = $this->getMock('Knp\Bundle\PaginatorBundle\Pagination\SlidingPagination', ['getPaginationData', 'getParams', 'getRoute', 'getPaginatorOptions'], [], '', false);
+        $pagination = m::mock(SlidingPagination::class);
 
-        $slidingPaginationMock->expects($this->at(0))
-            ->method('getPaginationData')
-            ->will(
-                $this->returnValue([
-                        'previous' => 1,
-                        'next' => 3
-                    ]
-                ));
+        $pagination->shouldReceive('getPaginationData')
+            ->once()
+            ->andReturn([
+                'previous' => '7',
+                'next'     => '9'
+            ]);
 
-        $slidingPaginationMock->expects($this->any())
-            ->method('getRoute')
-            ->will($this->returnValue('dummy_route'));
+        $pagination->shouldReceive('getRoute')
+            ->twice()
+            ->andReturn('dummy_route');
 
-        $slidingPaginationMock->expects($this->any())
-            ->method('getParams')
-            ->will($this->returnValue(array('direction' => 'desc')));
+        $pagination->shouldReceive('getQuery')
+            ->twice()
+            ->andReturn([
+                'direction' => 'asc',
+                'sort'      => 'sorting'
+            ]);
 
-        $slidingPaginationMock->expects($this->any())
-            ->method('getPaginatorOptions')
-            ->will($this->returnValue([
-                'pageParameterName' => 'page'
-            ]));
+        $this->router->shouldReceive('generate')
+            ->twice()
+            ->andReturn(
+                'http://local.domain.it/test/path-info?page=7',
+                'http://local.domain.it/test/path-info?page=9'
+            );
 
-        $requestMock = $this->getMock(
-            'Symfony\Component\HttpFoundation\Request',
-            ['getPathInfo'],
-            [
-                [
-                    'testParameter' => 'test',
-                    'page'          => 2
-                ]
-            ],
-            '',
-            true
+        $this->assertEquals(
+            '<link rel="prev" href="http://local.domain.it/test/path-info?page=7"><link rel="next" href="http://local.domain.it/test/path-info?page=9">',
+            $this->extension->renderPaginationMeta($pagination)
         );
-
-        $requestMock->expects($this->any())
-            ->method('getPathInfo')
-            ->will($this->returnValue('/test/path-info'));
-
-        $this->requestStackMock->expects($this->any())
-            ->method('getMasterRequest')
-            ->will($this->returnValue($requestMock));
-
-        $paginationMeta = new PaginationMetaExtension($this->requestStackMock, $this->routerMock, 'local.domain.it');
-
-        $this->assertEquals('<link rel="prev" href="http://local.domain.it/test/path-info"><link rel="next" href="http://local.domain.it/test/path-info?page=3">', $paginationMeta->renderPaginationMeta($slidingPaginationMock));
     }
 
     public function testSeoRobotsForPaginationValues()
     {
-        $page_parameters = array(
-            'first_page' => 1,
-            'random_page' => rand(2,20),
-        );
+        $pagination = new SlidingPagination([]);
 
-        $paginationMeta = new PaginationMetaExtension($this->requestStackMock, $this->routerMock,'local.domain.it');
+        $pagination->setCurrentPageNumber(1);
+        $this->assertEquals('<meta name="robots" content="index,follow" />', $this->extension->renderPaginationRobots($pagination));
 
-        $this->assertEquals('<meta name="robots" content="index,follow" />', $paginationMeta->renderPaginationRobots($page_parameters['first_page']));
-        $this->assertEquals('<meta name="robots" content="noindex,follow" />', $paginationMeta->renderPaginationRobots($page_parameters['random_page']));
-    }
-
-    public function testCanonicalTag()
-    {
-        $router = m::mock('Symfony\Bundle\FrameworkBundle\Routing\Router');
-        $router->shouldReceive('generate')
-            ->with('my_route', ['hello' => 'world', 'page' => 2], true)
-            ->andReturn('http://testing/canonical');
-
-        $extension = new PaginationMetaExtension($this->requestStackMock, $router, 'test.skuola.net');
-
-        $request = new Request(['page' => 2, 'other' => 'parameter'], [], [
-            '_route' => 'my_route',
-            '_route_params' => ['hello' => 'world'],
-        ]);
-
-        $this->assertEquals('<link href="http://testing/canonical" rel="canonical">', $extension->renderPaginationCanonical($request));
-    }
-
-    public function testFirstPageCanonicalTag()
-    {
-        $router = m::mock('Symfony\Bundle\FrameworkBundle\Routing\Router');
-        $router->shouldReceive('generate')
-            ->with('my_route', ['hello' => 'world'], true)
-            ->andReturn('http://testing/canonical');
-
-        $extension = new PaginationMetaExtension($this->requestStackMock, $router, 'test.skuola.net');
-
-        $request = new Request(['page' => 1, 'other' => 'parameter'], [], [
-            '_route' => 'my_route',
-            '_route_params' => ['hello' => 'world'],
-        ]);
-
-        $this->assertContains('href="http://testing/canonical" rel="canonical">', $extension->renderPaginationCanonical($request));
+        $pagination->setCurrentPageNumber(rand(2, 20));
+        $this->assertEquals('<meta name="robots" content="noindex,follow" />', $this->extension->renderPaginationRobots($pagination));
     }
 }
